2bb0df7cbf883edab5f6e022a6333619c86df8b9
rayanrahul1 <rayanrahul100@gmail.com>
2023-03-24 11:24:45 +0530

    first commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,18 @@
+void main(){
+  /// variable declaration
+  /// datatype variable_name = value;
+   String name = "Rahul";
+   int age = 24;
+   int phone = 7012263463;
+   double mark = 6.6;
+   String email = "rayanrahul100@gmail.com";
+
+
+   print("student details");
+   print("my name is $name");
+  print("i am $age yrs old");
+   print("my phone number is $phone");
+   print("my mark is $mark");
+   print("my email is $email");
+
+}
